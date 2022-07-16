9077f078fb85697ed011c247007828cf4c09090a
sseld <jrstephens09@gmail.com>
2022-07-16 10:36:08 -0400

    added basic menu and navigation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,16 @@
     import "../app.css";
 </script>
 
+
+<div class="navbar bg-base-100">
+    <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl" href="/">Dev Toolkit</a>
+    </div>
+    <div class="flex-none">
+        <ul class="menu menu-horizontal p-0">
+            <li><a href="/csv-json">CSV/JSON</a></li>
+        </ul>
+    </div>
+</div>
+
 <slot />
